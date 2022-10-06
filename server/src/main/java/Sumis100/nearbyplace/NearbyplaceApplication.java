a82ebb8dbeb4d76f2289b354935dfe92a83fccec
elandland <bodan99@gmail.com>
2022-10-06 15:16:16 +0900

    proxy settings

--- server/src/main/java/Sumis100/nearbyplace/NearbyplaceApplication.java
+++ server/src/main/java/Sumis100/nearbyplace/NearbyplaceApplication.java
@@ -1,9 +1,11 @@
 package Sumis100.nearbyplace;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class NearbyplaceApplication {
 
 	public static void main(String[] args) {
