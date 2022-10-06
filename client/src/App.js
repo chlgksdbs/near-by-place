897d321adeaf9ca24f455967d33c8993b6ed47b2
최한윤 <chlgksdbs98@naver.com>
2022-10-06 15:04:18 +0900

    proxy settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,27 @@
+// src/main/frontend/src/App.js
+
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+function App() {
+   const [hello, setHello] = useState('')
+
+    useEffect(() => {
+        axios.get('/api/hello')
+        .then(response => setHello(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
+}
+
+export default App;
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +47,4 @@ function App() {
 }
 
 export default App;
+*/
