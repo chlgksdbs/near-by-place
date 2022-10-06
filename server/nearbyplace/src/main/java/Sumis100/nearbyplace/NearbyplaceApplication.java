cc63eefbe65907940a7d3b64ed79df2d45d24e9a
elandland <bodan99@gmail.com>
2022-10-06 14:38:22 +0900

    Near by Place Project

---
+++ server/nearbyplace/src/main/java/Sumis100/nearbyplace/NearbyplaceApplication.java
@@ -0,0 +1,13 @@
+package Sumis100.nearbyplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyplaceApplication.class, args);
+	}
+
+}
---
+++ server/nearbyplace/src/test/java/Sumis100/nearbyplace/NearbyplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package Sumis100.nearbyplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
