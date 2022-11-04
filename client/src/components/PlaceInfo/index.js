e0a80833889250c8a3660d49f757d244ef5c3df9
kykapple <kykapple@naver.com>
2022-11-05 01:20:31 +0900

    feat: 디테일 페이지 장소 상세 정보 레이아웃 구현

--- client/src/components/PlaceInfo/index.js
+++ client/src/components/PlaceInfo/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 import TestImg from "../../assets/test.png";
-import { Container, MainImage, SubImage, SubImageContainer } from "./style";
+import {
+    Container, Detail,
+    DetailBox, DetailTitleBox,
+    Grade,
+    InfoContainer,
+    MainImage,
+    SubImage,
+    SubImageContainer,
+    Title,
+    TitleBox
+} from "./style";
 
 const testPlace = {
     images: [ TestImg, TestImg, TestImg, TestImg, ],
     name: "보정동 농구장",
     address: "경기도 용인시 기흥구 보정동",
-    description: "3x3 경기, 5x5 풀코트 경기 가능",
+    description: " 3x3 경기, 5x5 풀코트 경기 가능 3x3 경기, 5x5 풀코트 경기 가능 3x3 경기, 5x5 풀코트 경기 가능",
     grade_avg: 4.8,
 };
 
@@ -20,6 +30,24 @@ const PlaceInfo = () => {
                     .slice(1)
                     .map(place => <SubImage src={place} />)}
             </SubImageContainer>
+            <InfoContainer>
+                <TitleBox>
+                    <Title>{testPlace.name}</Title>
+                    <Grade>{testPlace.grade_avg}</Grade>
+                </TitleBox>
+                <DetailBox>
+                    <DetailTitleBox>
+                        <Detail>
+                            <div>주소</div>
+                            <div>{testPlace.address}</div>
+                        </Detail>
+                        <Detail>
+                            <div>설명</div>
+                            <div>{testPlace.description}</div>
+                        </Detail>
+                    </DetailTitleBox>
+                </DetailBox>
+            </InfoContainer>
         </Container>
     )
 };
--- client/src/components/PlaceInfo/style.js
+++ client/src/components/PlaceInfo/style.js
@@ -21,3 +21,53 @@ export const SubImage = styled.img`
     margin-right: 20px;
 `;
 
+export const InfoContainer = styled.div`
+    padding-bottom: 20px;
+    max-width: 550px;
+    border-bottom: 2px solid #000;
+`;
+
+export const TitleBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 25px;
+    padding-bottom: 10px;
+    border-bottom: 2px solid #000;
+`;
+
+export const Title = styled.div`
+    font-size: 22px;
+    font-weight: 400;
+`;
+
+export const Grade = styled.div`
+    font-size: 22px;
+    font-weight: 540;
+    color: #FF9900;
+    margin-left: 10px;
+`;
+
+export const DetailBox = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const DetailTitleBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 100px;
+    margin-top: 10px;
+`;
+
+export const Detail = styled.div`
+    display: flex;
+    flex-direction: row;
+    
+    & > div {
+        min-width: 30px;
+        font-size: 13px;
+        font-weight: 600;
+        margin-right: 40px;
+    }
+`;
